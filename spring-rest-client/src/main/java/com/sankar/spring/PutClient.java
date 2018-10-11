8eede4894356b75c7148f539e161ed9b2b5dfe57
wavehunt <alurisankar@gmail.com>
2018-10-11 12:32:26 +0530

    Add files via upload
    Added Update and Delete functionalities

---
+++ spring-rest-client/src/main/java/com/sankar/spring/PutClient.java
@@ -0,0 +1,36 @@
+package com.sankar.spring;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.sankar.spring.entity.Emp;
+
+public class PutClient {
+
+	public static void main(String[] args) {
+		
+		RestTemplate rt = new RestTemplate();
+		
+		//-------------------------------
+		// This is to UPDATE an existing employee
+		//-------------------------------
+		Emp emp = new Emp(124,"Chat","Outskirts", 110000);
+				
+		HttpHeaders headers1 = new HttpHeaders();
+		headers1.set("Accept", "text/plain");
+		headers1.set("Content-Type", "application/json");
+		
+		HttpEntity req1 = new HttpEntity<>(emp,headers1);
+		
+		ResponseEntity<String> resp1=rt.exchange("http://localhost:8081/emp/update", HttpMethod.PUT, req1, 
+				
+				String.class);
+		
+		System.out.println(resp1.getBody());		
+
+	}
+
+}
